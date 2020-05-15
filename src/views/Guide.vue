642671dfc2e35bc2129c7cec916e2b0b4a295e34
Skyler Fly-Wilson <skyfly200@gmail.com>
2020-05-14 19:37:37 -0600

    add sub views for the guide

--- src/views/Guide.vue
+++ src/views/Guide.vue
@@ -2,27 +2,31 @@
   .guide
     v-navigation-drawer(permanent).menu
       v-list(dense nav)
-        v-list-item
-          v-list-item-content
-            v-list-item-title(class="title") Welcome
-        v-list-item
+        v-list-item(to="/guide/minting")
           v-list-item-content
             v-list-item-title(class="title") Minting
-        v-list-item
+        v-list-item(to="/guide/types")
           v-list-item-content
             v-list-item-title(class="title") Types
+        v-list-item(to="/guide/graphics")
+          v-list-item-content
+            v-list-item-title(class="title") Graphics
     v-sheet.content
-      h1 Welcome to the Guide
-      h2 Learn more about tiny boxes
-      h3 Get started here
-      h3 {{ topic }}
+      Welcome(v-if="!topic")
+      h1(v-else-if="topic === 'minting'") Minting
+      h1(v-else-if="topic === 'types'") Types
+      h1(v-else-if="topic === 'graphics'") Graphics
+      .err404(v-else) 
+        h1.error 404 Error: Page {{ topic }} not found in guide
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import Welcome from "./guide/Welcome.vue";
 
 export default Vue.extend({
   name: "Guide",
+  components: { Welcome },
   computed: {
     topic() {
       return this.$route.params.topic;
