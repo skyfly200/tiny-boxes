1ce0778c13bb58097cdc8eb0763e936b03b7873c
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2020-05-27 13:13:57 -0600

    organize sliders

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -4,33 +4,36 @@
       v-row
         v-col(align="center")
           v-card
-            .token-stats
-              h1 Token {{ id }}
+            v-card-title.token-stats
+              h1 Token {{ "#" + id }}
             v-divider
-            .token-graphic
+            v-card-text.token-graphic
               Token(:id="id" :data="data")
+            v-card-actions
+              v-btn(@click="loadToken") Preview
+              v-spacer
+              v-btn(@click="") Mint
         v-col
           h1 Create a TinyBox
           v-form(v-model="valid").create-form
-            v-text-field(v-model="seed" outlined label="Seed" required)
+            v-text-field(v-model="seed" outlined label="RNG Seed" required)
             h3 Counts
             .counts
               v-slider(v-model="counts[0]" thumb-label label="Color Count" required)
               v-slider(v-model="counts[1]" thumb-label label="Shape Count" required)
-            h3 Dials
             .dials
+              h3 Position
               .position
                 v-text-field(v-model="dials[0]" outlined label="X Position" required)
                 v-text-field(v-model="dials[1]" outlined label="Y Position" required)
+              h3 Size
               v-slider(v-model="dials[2]" thumb-label label="Width" required)
-              v-slider(v-model="dials[3]" thumb-label label="Width Variation" required)
               v-slider(v-model="dials[4]" thumb-label label="Height" required)
+              h3 Variation
+              v-slider(v-model="dials[3]" thumb-label label="Width Variation" required)
               v-slider(v-model="dials[5]" thumb-label label="Height Variation" required)
+              h3 Other
               v-slider(v-model="dials[6]" thumb-label label="Density" required)
-            .form-buttons
-              v-btn(@click="loadToken") Preview
-              v-spacer
-              v-btn(@click="") Mint
     
 </template>
 
@@ -102,8 +105,6 @@ export default Vue.extend({
       color: #121212
       background-color: #121212
       border: none
-  .form-buttons
-    display: flex
   .features
     margin: 2vh 1vw
     display: flex
