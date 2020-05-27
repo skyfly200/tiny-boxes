7b7b1aa69e267943e46cea854a68b569f2655010
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2020-05-27 14:00:09 -0600

    dynamic form items

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -16,24 +16,11 @@
         v-col
           h1 Create a TinyBox
           v-form(v-model="valid").create-form
-            v-text-field(v-model="seed" outlined label="RNG Seed" required)
-            h3 Counts
-            .counts
-              v-slider(v-model="counts[0]" thumb-label label="Color Count" required)
-              v-slider(v-model="counts[1]" thumb-label label="Shape Count" required)
-            .dials
-              h3 Position
-              .position
-                v-text-field(v-model="dials[0]" outlined label="X Position" required)
-                v-text-field(v-model="dials[1]" outlined label="Y Position" required)
-              h3 Size
-              v-slider(v-model="dials[2]" thumb-label label="Width" required)
-              v-slider(v-model="dials[4]" thumb-label label="Height" required)
-              h3 Variation
-              v-slider(v-model="dials[3]" thumb-label label="Width Variation" required)
-              v-slider(v-model="dials[5]" thumb-label label="Height Variation" required)
-              h3 Other
-              v-slider(v-model="dials[6]" thumb-label label="Density" required)
+            .section(v-for="section of sections" :key="section.title")
+              h3 {{ section.title }}
+              v-slider(v-for="option of section.options" v-model="values[option.key]" thumb-label :label="option.label" required :min="option.min" :max="option.max")
+                template(v-slot:append)
+                  v-text-field(v-model="values[option.key]" hide-details single-line type="number" style="width: 60px")
     
 </template>
 
@@ -62,7 +49,10 @@ export default Vue.extend({
     },
     loadToken: async function() {
       await this.getNext();
-      this.$store.state.contracts.tinyboxes.methods.perpetualRender(this.id, this.seed, this.counts, this.dials)
+      const v = this.values;
+      const counts = [v.colors, v.shapes];
+      const dials = [v.x, v.y, v.width, v.widthVariance, v.height, v.heightVariance, v.density];
+      this.$store.state.contracts.tinyboxes.methods.perpetualRender(this.id, v.seed, counts, dials)
         .call()
         .then((result: any) => {
           this.data = result;
@@ -73,15 +63,128 @@ export default Vue.extend({
         });
     }
   },
-  data: () => ({
-    loading: true,
-    data: null as (null | object),
-    valid: true,
-    id: 0,
-    seed: 1234567890,
-    counts: [7,11],
-    dials: [200,200,200,200,200,200,7]
-  })
+  data: function() {
+    return {
+      loading: true,
+      data: null as (null | object),
+      valid: true,
+      id: 0,
+      values: {
+        seed: 1234,
+        colors: 7,
+        shapes: 11,
+        x: 200,
+        y: 200,
+        width: 200,
+        height: 200,
+        widthVariance: 200,
+        heightVariance: 200,
+        density: 7
+      },
+      sections: [
+        {
+          title: "RNG",
+          options: [
+            {
+              label: "Seed",
+              key: "seed",
+              type: "",
+              min: 0,
+              max: 6800
+            }
+          ]
+        },
+        {
+          title: "Counts",
+          options: [
+            {
+              label: "Colors",
+              key: "colors",
+              type: "number",
+              min: 0,
+              max: 100
+            },
+            {
+              label: "Shapes",
+              key: "shapes",
+              type: "number",
+              min: 0,
+              max: 77
+            }
+          ]
+        },
+        {
+          title: "Position",
+          options: [
+            {
+              label: "X",
+              key: "x",
+              type: "number",
+              min: -500,
+              max: 500
+            },
+            {
+              label: "Y",
+              key: "shapes",
+              type: "number",
+              min: -500,
+              max: 500
+            }
+          ]
+        },
+        {
+          title: "Size",
+          options: [
+            {
+              label: "Width",
+              key: "width",
+              type: "number",
+              min: 0,
+              max: 500
+            },
+            {
+              label: "Height",
+              key: "height",
+              type: "number",
+              min: 0,
+              max: 500
+            }
+          ]
+        },
+        {
+          title: "Variance",
+          options: [
+            {
+              label: "Width",
+              key: "widthVariance",
+              type: "number",
+              min: 0,
+              max: 500
+            },
+            {
+              label: "Height",
+              key: "heightVariance",
+              type: "number",
+              min: 0,
+              max: 500
+            }
+          ]
+        },
+        {
+          title: "Other",
+          options: [
+            {
+              label: "Density",
+              key: "density",
+              type: "number",
+              min: 0,
+              max: 25
+            }
+          ]
+        }
+      ]
+    }
+  }
 });
 </script>
 
