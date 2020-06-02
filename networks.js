3cdab38ab5c1bd69ef5870145a50e353d7b7ef97
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2020-06-02 12:45:39 -0600

    add rinkby network config

--- networks.js
+++ networks.js
@@ -3,10 +3,17 @@ module.exports = {
     development: {
       protocol: 'http',
       host: 'localhost',
-      port: 8545,
-      gas: 5000000,
+      port: 7545,
+      gas: 6721975,
+      gasPrice: 2e10,
+      networkId: '*',
+    },
+    rinkeby: {
+      protocol: 'https',
+      host: 'rinkeby.infura.io/v3/517826b8f74845aa8b16945c61780ca1',
+      gas: 6721975,
       gasPrice: 5e9,
       networkId: '*',
     },
   },
-};
+}
