22a812e465d70352a81484ad012c0524dc1efad3
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2020-06-12 00:30:55 -0600

    configure hot reload for solidity w/ open zeppelin

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,18 @@
 module.exports = {
-  transpileDependencies: ["vuetify"]
-};
+  transpileDependencies: ['vuetify'],
+  chainWebpack: (config) => {
+    config.module
+      .rule('sol')
+      .test(/\.sol$/)
+      .use('json-loader')
+      .loader('json-loader')
+      .end()
+      .use('@openzeppelin/solidity-loader')
+      .loader('@openzeppelin/solidity-loader')
+      .tap((options) => {
+        options.network = 'development',;
+        options.disabled = false;
+        return options;
+      })
+  },
+}
