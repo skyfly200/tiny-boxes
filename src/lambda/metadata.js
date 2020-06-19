275c3ca6d69b431fd71ffa9baf583bd762f42fe0
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2020-06-19 17:28:13 -0600

    add metadat object example

--- src/lambda/metadata.js
+++ src/lambda/metadata.js
@@ -6,6 +6,16 @@ const { PINATA_API_KEY, PINATA_API_SECRET, WALLET_PRIVATE_KEY } = process.env
 exports.handler = function (event, context, callback) {
   const id = event.queryStringParameters.id
 
+  let metadata = {
+    description:
+      'Friendly OpenSea Creature that enjoys long swims in the ocean.',
+    external_url: 'https://openseacreatures.io/3',
+    image:
+      'https://storage.googleapis.com/opensea-prod.appspot.com/puffs/3.png',
+    name: 'Dave Starbelly',
+    attributes: [],
+  }
+
   return {
     statusCode: 200,
     body: `Token: ${id}`,
