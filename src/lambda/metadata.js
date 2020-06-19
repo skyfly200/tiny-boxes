816d7dd095a2f522ff52ce1dd5d622060a82e0f6
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2020-06-19 15:55:22 -0600

    add metadata API starter logic add .env vars and ignore

--- src/lambda/metadata.js
+++ src/lambda/metadata.js
@@ -1,5 +1,13 @@
-// require("dotenv").config();
-// import querystring from "querystring";
-// const domain = process.env.DOMAIN;
+require('dotenv').config()
+import querystring from 'querystring'
+const domain = process.env.DOMAIN
+const { PINATA_API_KEY, PINATA_API_SECRET, WALLET_PRIVATE_KEY } = process.env
 
-exports.handler = function(event, context, callback) {};
+exports.handler = function (event, context, callback) {
+  const id = event.queryStringParameters.id
+
+  return {
+    statusCode: 200,
+    body: `Token: ${id}`,
+  }
+}
