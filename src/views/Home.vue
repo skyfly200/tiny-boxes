56b60350fad4865c987c770e9980ad844ae694e8
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2020-06-27 12:18:32 -0600

    update home page content

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,10 +5,54 @@
         v-col(align="center")
           h1.title TinyBoxes
           h3 Boxes Upon Boxes
-          h3 Art is stored on chain!
-          .call-to-action
-            v-btn.btn(color="primary" large to="/list") Play Now
-            v-btn.btn(color="tertiary" large to="/about") Learn More
+          h3 Fully on chain, animated NFTs
+          v-btn.btn(color="primary" large to="/list") Explore
+          v-btn.btn(color="tertiary" large to="/about") Learn More
+      v-row(cols=6)
+        v-col(align="center")
+          v-card
+            v-card-text
+              h1.title On Chain Generative Art
+              h3 The Immutible Decentralized Perpetual Renderer 
+              h3 That means art thats accesible forever and fully owned by you
+            v-card-actions.call-to-action
+              v-btn.btn(color="primary" large to="/guide") The Perpetual Renderer
+      v-row(cols=6)
+        v-col(align="center")
+          v-card
+            v-card-text
+              h1.title Creative Options + Mystery
+              h3 Tweak the sliders, explore the posibilities
+              h3 You never know what you will get, thanks to a dash of RNG on minting
+            v-card-actions.call-to-action
+              v-btn.btn(color="primary" large to="/guide") Create a Box
+      v-row(cols=6)
+        v-col(align="center")
+          v-card
+            v-card-text
+              h1.title Chainlink VRF
+              h3 Using the latest secure RNG solution from Chainlink
+              h3 Assuring your tokens are minted fairly with truly random values
+            v-card-actions.call-to-action
+              v-btn.btn(color="primary" large to="/guide") Chainlink VRF
+      v-row(cols=6)
+        v-col(align="center")
+          v-card
+            v-card-text
+              h1.title Accepting LINK
+              h3 Our contract directly accepts LINK token to mint TinyBoxes
+              h3 A first for NFT projects
+            v-card-actions.call-to-action
+              v-btn.btn(color="primary" large to="/guide") Mint with LINK
+      v-row(cols=6)
+        v-col(align="center")
+          v-card
+            v-card-text
+              h1.title OpenSea Secondary Market
+              h3 Ready for OpenSea integration
+              h3 Featuring animations
+            v-card-actions.call-to-action
+              v-btn.btn(color="primary" large to="/guide") OpenSea
 </template>
 
 <script>
@@ -35,10 +79,10 @@ export default {
         "579032",
         "16426533",
         "14842158",
-        "568390"
-      ]
-    }
-  })
+        "568390",
+      ],
+    },
+  }),
 };
 </script>
 
@@ -65,6 +109,7 @@ export default {
   .title
     font-size: 4rem
   .call-to-action
+    align: center
     margin-top: 5vh
     color: #121212
     .btn
