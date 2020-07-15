7432c6768be543e1152265f52bb972c35b9837c1
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2020-07-15 15:45:59 -0600

    header with buffer

--- contracts/TinyBoxesRenderer.sol
+++ contracts/TinyBoxesRenderer.sol
@@ -106,18 +106,20 @@ library TinyBoxesRenderer {
      * @dev render the header of the SVG markup
      * @return header string
      */
-    function _generateHeader() internal pure returns (string memory header) {
+    function _generateHeader() internal view returns (string memory) {
+        bytes memory buffer = new bytes(8192);
+
         string memory xmlVersion = '<?xml version="1.0" encoding="UTF-8"?>\n';
         string memory doctype = '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">\n';
         string memory openingTag = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="100%" height="100%" viewBox="0 0 2600 2600" style="stroke-width:0; background-color:#121212;">\n\n';
         string memory symbols = '<symbol id="quad3">\n<symbol id="quad2">\n<symbol id="quad1">\n<symbol id="quad0">\n\n';
 
-        StringUtilsLib.slice[] memory parts;
-        parts[0] = xmlVersion.toSlice();
-        parts[1] = doctype.toSlice();
-        parts[2] = openingTag.toSlice();
-        parts[3] = symbols.toSlice();
-        return StringUtilsLib.join((" ").toSlice(), parts);
+        buffer.append(xmlVersion);
+        buffer.append(doctype);
+        buffer.append(openingTag);
+        buffer.append(symbols);
+
+        return buffer.toString();
     }
 
     /**
