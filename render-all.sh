9f6b57329aae81564f6297d43cd8f8fd2423d94b
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2020-11-12 23:23:34 -0700

    add -i all flag in script

--- render-all.sh
+++ render-all.sh
@@ -7,9 +7,10 @@ LINK="0x01be23585060835e02b77ef475b0cc51aa1e0709"
 FEED="0x3Af8C569ab77af5230596Acf0E8c2F9351d24C38"
 
 ## deploy only on calls with a -d flag present
-while getopts "d:a:" arg; do
+while getopts "d:i:a:" arg; do
     case $arg in
         d) DEPLOY="set";;
+        i) ALL="set";;
         a) ADDRESS=$OPTARG;;
     esac
 done
@@ -33,10 +34,17 @@ if [ -z "$ADDRESS" ]
         echo "Calling against contract @ $ADDRESS"
         ## delete oz lock file ./.openzeppelin/.lock
         rm -f ./.openzeppelin/.lock
-        # adjust max animation value here
-        for ANIMATION in {0..7}
-        do  
-            # render svg at
-            npx oz call --method tokenTest -n rinkeby --args "12345, [10,10], [100,100,2,2,111,222,333,444,2,750,1200,2400,100], [true,true,true], $ANIMATION, 0" --to "$ADDRESS" > "./frames/Anim-$ANIMATION.svg"
-        done
+        if [ -z "$ALL" ]
+            then
+                # render svg at
+                npx oz call --method tokenTest -n rinkeby --args "12345, [10,10], [100,100,2,2,111,222,333,444,2,750,1200,2400,100], [true,true,true], $ANIMATION, 0" --to "$ADDRESS" > "./frames/Anim-$ANIMATION.svg"
+            else
+                # adjust max animation value here
+                for ANIMATION in {0..7}
+                do  
+                    # render svg at
+                    npx oz call --method tokenTest -n rinkeby --args "12345, [10,10], [100,100,2,2,111,222,333,444,2,750,1200,2400,100], [true,true,true], $ANIMATION, 0" --to "$ADDRESS" > "./frames/Anim-$ANIMATION.svg"
+                done
+        fi
+        
 fi
