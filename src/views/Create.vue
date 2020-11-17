a944c5e74a7030023e83e10b813fa4cd4964e7c0
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2020-11-17 07:42:31 -0700

    fix the not a module error

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -98,7 +98,7 @@
 <script lang="ts">
 import Vue from "vue";
 import { mapGetters } from "vuex";
-import { tinyboxesAddress } from "../tinyboxes-contract";
+import "../tinyboxes-contract";
 import { sections } from "./create-form";
 import Token from "@/components/Token.vue";
 
