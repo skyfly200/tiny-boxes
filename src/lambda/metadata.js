f7a43814a9c21523864170f32c63990d7d32d921
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2020-11-18 09:30:56 -0700

    wrap bloc looup chain in a promise

--- src/lambda/metadata.js
+++ src/lambda/metadata.js
@@ -5,6 +5,7 @@ import pinataSDK from '@pinata/sdk'
 import axios from 'axios'
 import FormData, { Stream } from 'form-data'
 import { tinyboxesABI } from '../tinyboxes-contract'
+import { resolve } from 'path'
 dotenv.config()
 
 const {
@@ -16,11 +17,42 @@ const {
   CONTRACT_ADDRESS,
 } = process.env
 
+// init web3 provider and load contract
+var web3 = new Web3(WEB3_PROVIDER_ENDPOINT)
+console.log(CONTRACT_ADDRESS);
+const tinyboxesContract = new web3.eth.Contract(
+  tinyboxesABI,
+  CONTRACT_ADDRESS,
+)
+
 const generateResponse = (body, statusCode) => {
   return {
     statusCode: statusCode,
     body: JSON.stringify(body),
   }
+}
+
+function lookupMintedBlock(id) {
+  return new Promise((resolve, reject) => {
+    web3.eth
+      .subscribe('logs', {
+        address: CONTRACT_ADDRESS,
+        fromBlock: 0,
+        topics: [
+          '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',
+          '0x0000000000000000000000000000000000000000000000000000000000000000',
+          null,
+          '0x' + id.toString(16).padStart(64, '0'),
+        ],
+      })
+      .on("data", async (log) => {
+        console.log('...Minted Block...')
+        web3.eth.getBlock(log.blockNumber, (err, response) => {
+          if (err) reject(err);
+          else resolve(response);
+        })
+      });
+  })
 }
 
 exports.handler = async (event, context) => {
@@ -37,17 +69,9 @@ exports.handler = async (event, context) => {
       console.log('Undefined ID parameter is required')
       return generateResponse('Undefined ID parameter is required', 204)
     }
-    console.log('Loading Web3')
-
-    // init web3 provider
-    var web3 = new Web3(WEB3_PROVIDER_ENDPOINT)
-    const tinyboxesContract = new web3.eth.Contract(
-      tinyboxesABI,
-      CONTRACT_ADDRESS,
-    )
 
     // check token exists and get owner
-    console.log('Checking token of ID exists')
+    console.log('Checking token of ID ', id, ' exists')
     const owner = await tinyboxesContract.methods.ownerOf(id).call()
     if (owner === '0x0000000000000000000000000000000000000000') {
       // complain if token is missing
@@ -64,45 +88,35 @@ exports.handler = async (event, context) => {
     console.log('...Animation...')
     const animatonPromise = tinyboxesContract.methods.tokenArt(id,true).call()
     console.log('...Mined Block Number...')
-    let mintedPromise
-    web3.eth
-      .subscribe('logs', {
-        address: CONTRACT_ADDRESS,
-        fromBlock: 0,
-        topics: [
-          '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',
-          '0x0000000000000000000000000000000000000000000000000000000000000000',
-          null,
-          '0x' + id.toString(16).padStart(64, '0'),
-        ],
-      })
-      .on("data", async (log) => {
-        console.log('...Minted Block...')
-        console.log(log);
-        parseInt(log.topics[3], 16);
-        mintedPromise = web3.eth.getBlock(blockNumResp)
-      });
+    const blockPromist = lookupMintedBlock(id);
+    
 
     // await token data
     console.log("Awaiting requests...");
-    const [data, art, animation, minted] = ['', '', '', '']
-    await Promise.all([dataPromise, artPromise, animatonPromise, mintedPromise])
-      .then((values) => console.log(values))
-      .catch((err) => console.error(err));
+    let [data, art, animation, block] = ['', '', '', '']
+    await dataPromise
+      .then(() => data = result)
+      .catch((err) => console.error(err))
+    console.log("Data: ",data);
+    await artPromise
+      .then((result) => art = result)
+      .catch((err) => console.error(err))
+    console.log("Art: ",art);
+    // animation = await animatonPromise
+    //   .catch((err) => console.error(err))
+    // console.log("Animation: ",animation);
+
+    //if (data === undefined || art === undefined || animation === undefined) return generateResponse('Server Error', 500)
     
     // console.log('Lookup Complete!')
-    // console.log(data);
-    // console.log(art);
-    // console.log(animation);
-    // console.log(minted);
 
     // convert art stream from SVG to PNG
 
     // build MP4 stream of animation
 
     // load Pinata SDK
+    console.log('Connecting to Pinata SDK...')
     const pinata = pinataSDK(PINATA_API_KEY, PINATA_API_SECRET)
-    console.log('Uploading art to IPFS...')
     const url = `https://api.pinata.cloud/pinning/pinFileToIPFS`
 
     // // build form data for the IPFS pin
@@ -120,18 +134,20 @@ exports.handler = async (event, context) => {
     // })
     // const ipfsResp = await ipfsPromise
 
-    const artStream = Readable.from(art)
-    const animationStream = Readable.from(animation)
-    const imageHash = (await pinata.pinFileToIPFS(artStream)).IpfsHash
-    const animationHash = (await pinata.pinFileToIPFS(animationStream)).IpfsHash
-    console.log('IPFS Hashes: ')
-    console.log(imageHash)
-    console.log(animationHash)
+    // console.log('Creating Streams from Strings')
+    // const artStream = Readable.from(art)
+    // const animationStream = Readable.from(animation)
+    // console.log('Uploading Streams to IPFS')
+    // const imageHash = (await pinata.pinFileToIPFS(artStream)).IpfsHash
+    // const animationHash = (await pinata.pinFileToIPFS(animationStream)).IpfsHash
+    // console.log('IPFS Hashes: ')
+    // console.log(imageHash)
+    // console.log(animationHash)
     // const imageHash = ''
     // const animationHash = ''
 
     // build the metadata object from the token data and IPFS hashes
-    // let metadata = {
+    let metadata = {}
     //   name: 'TinyBox #' + id,
     //   description:
     //     'A scattering of tiny boxes, Aranged in patterns ranging from mundane to magnificent.',
@@ -221,7 +237,7 @@ exports.handler = async (event, context) => {
     // // update token with the metadataHash
     // // can happen after response
 
-    // return generateResponse(metadata, 200)
+    return generateResponse(metadata, 200)
   } catch (err) {
     console.log(err)
     return generateResponse('Server Error', 500)
