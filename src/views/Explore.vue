50390064283fb6d598dc32581aeacd98aacd16e3
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2020-12-04 01:20:42 -0700

    strip down explore comp

--- src/views/Explore.vue
+++ src/views/Explore.vue
@@ -1,19 +1,19 @@
 <template lang="pug">
   .token-explorer
     v-container(fluid)
+      v-row(width="100%")
+        .heading(align="center")
+          .title Explore All The Boxy Variety
+          p Hundreds of googols of possibilities
       v-row(flex)
-        .title Explore The Boxy Variety
-        v-col(align="center" cols="12" md="5" offset-md="1")
-          v-card(max-height="90vh").token-preview
-            v-card-title.token-stats
-              p Put Stats Here
-            v-divider
-            v-card-text.token-graphic
-              v-fade-transition(mode="out-in")
-                v-skeleton-loader(v-if="loading" tile type="image")
-                Token(v-else :id="0" :data="data")
-            v-card-actions
-              v-btn(@click="") Actions!
+        v-card(height="20vh" width="20vh" v-for="v,i in tokens").token-permutation
+          v-card-title.token-stats {{i}}
+          v-divider
+          v-card-text.token-graphic
+            v-skeleton-loader(v-if="loading" tile type="image")
+            Token(:id="i" :data="v")
+          v-card-actions
+            v-btn(@click="") Mint
 </template>
 
 <script lang="ts">
@@ -38,16 +38,7 @@ export default Vue.extend({
     t.loadFormDefaults();
   },
   methods: {
-    formatHash(account: string) {
-      return "0x" + account.slice(2, 6) + "...." + account.slice(-4);
-    },
-    update: async function() {
-      const t = this as any;
-      this.$router.push({ path: "/explore", query: t.values })
-      if (t.form.valid) return t.loadToken();
-    },
-    assemblePalette: function() {
-      const v = (this as any).values;
+    assemblePalette: function(v: any) {
       return [
         v.hue,
         v.saturation,
@@ -57,9 +48,8 @@ export default Vue.extend({
         v.shades
       ];
     },
-    assembleDials: function() {
+    assembleDials: function(v: any) {
       const t = this as any;
-      const v = t.values;
       return [
         v.x,
         v.y,
@@ -76,108 +66,37 @@ export default Vue.extend({
         v.mirrorAdv ? v.scale : (!v.mirrorC ? (!v.mirrorB ? 400 : 200) : 100),
       ];
     },
-    getPrice: function() {
-      return this.$store.state.contracts.tinyboxes.methods
-        .currentPrice()
-        .call();
-    },
     loadFormDefaults: function() {
       const t = this as any;
       // set values to default
       Object.assign(t.values, t.defaults);
-      // overwrite with any url query params
-      Object.assign(t.values, t.$route.query);
-      t.update();
-    },
-    randomizeForm: function() {
-      const t = this as any;
-      t.randomizeAll();
-      t.update()
-        .catch((err: any) => {
-          console.error("Invalid Box Options - Call Reverted: ", err)
-          console.log("Retrying Randomize")
-          t.randomizeForm()
-        });
-    },
-    randomizeSection: function(section: number) {
-      const t = this as any;
-      t.randomize(section);
-      t.update()
-        .catch((err: any) => {
-          console.error("Invalid Box Options - Call Reverted: ", err)
-          console.log("Retrying Randomize")
-          t.randomizeForm()
-        });
-    },
-    randomize: function(section: number) {
-      const t = this as any;
-      const randomSettings: any = {};
-      const s = t.sections[section];
-      if (s.rand)
-        for (const o of s.options) {
-          if (o.rand !== false && !t.values[o.hide] && t.values[o.show] !== false) {
-            const range = o.rand ? o.rand : o.range;
-            switch (o.type) {
-              case "switch":
-                randomSettings[o.key] = Math.random() > (o.randWeight ? o.randWeight : 0.5);
-                break;
-              case "range-slider":
-                randomSettings[o.key] = [range, range]
-                  .map((r) => t.between(r))
-                  .sort();
-                break;
-              default:
-                randomSettings[o.key] = t.between(range);
-                break;
-            }
-          }
-        }
-      Object.assign(t.values, randomSettings);
-    },
-    randomizeAll: function() {
-      const t = this as any;
-      const randomSettings: any = {};
-      for (const s of t.sections)
-        if (s.rand)
-          for (const o of s.options) {
-            if (o.rand !== false && !t.values[o.hide] && t.values[o.show] !== false) {
-              const range = o.rand ? o.rand : o.range;
-              switch (o.type) {
-                case "switch":
-                  randomSettings[o.key] = Math.random() > (o.randWeight ? o.randWeight : 0.5);
-                  break;
-                case "range-slider":
-                  randomSettings[o.key] = [range, range]
-                    .map((r) => t.between(r))
-                    .sort();
-                  break;
-                default:
-                  randomSettings[o.key] = t.between(range);
-                  break;
-              }
-            }
-          }
-      Object.assign(t.values, randomSettings);
+      t.loadTokens();
     },
     between: function(range: any) {
       return Math.floor(
         Math.random() * (range.max - range.min + 1) + range.min
       );
     },
-    loadToken: function() {
+    loadTokens: async function() {
+      this.loading = true;
+      for (let t=0;t<this.count;t++) {
+        const mod: any = {};
+        mod[this.attribute] = t;
+        Object.assign(this.values, mod);
+        this.$set(this.tokens, t, await this.loadToken(this.values));
+      }
+      this.loading = false;
+    },
+    loadToken: function(v: any) {
       return new Promise((resolve, reject) => {
         const t = this as any;
-        t.loading = true;
-        const v = t.values;
-        const palette = t.assemblePalette();
-        const dials = t.assembleDials();
-        console.log(v)
+        const palette = t.assemblePalette(v);
+        const dials = t.assembleDials(v);
         this.$store.state.contracts.tinyboxes.methods
           .tokenTest(v.seed.toString(), v.shapes, palette, dials, v.animation, v.animate)
           .call()
           .then((result: any) => {
             t.data = result;
-            t.loading = false;
             resolve(result);
           })
           .catch((err: any) => {
@@ -191,7 +110,9 @@ export default Vue.extend({
   data: function() {
     return {
       loading: true,
-      data: null as object | null,
+      attribute: "shapes",
+      count: 10,
+      tokens: [] as any,
       values: {} as any,
       defaults: {
         seed: Date.now(),
@@ -225,46 +146,8 @@ export default Vue.extend({
 </script>
 
 <style lang="sass">
-.v-chip__content
-  span
-    color: #FFF !important
-.content
-  margin-top: 35vh
-.id
-  font-size: 2rem
-.token-loading
-  padding-top: 40hv
 .token-graphic
   padding: 0 !important
-.token-preview .token
-  height: 60vh
-.sold-out
-  margin-top: 1rem
-.form-buttons, .price-tag
-  display: flex
-.v-expansion-panel-header .rand-btn
-  flex: 0 0 auto !important
-  margin-right: 10px
-.theme--dark.v-input
-  margin: 0 15px
-  width: 100%
-  input, textarea
-    color: #121212
-    background-color: #121212
-    border: none
-.v-input.switch
-  width: auto
-.section-title
-  color: #fff
-.section-content .v-expansion-panel-content__wrap
-  display: flex
-  flex-wrap: wrap
-.dialog .v-card__text
-  padding: 0
-  .message
-    margin: 1rem
-.dialog-error .v-card__text,  .v-alert
-  margin: 1rem
 .container
   padding-left: 0
   padding-right: 0
@@ -272,6 +155,4 @@ export default Vue.extend({
     margin-right: 0
     @media(max-width: 700px)
       margin: 0
-.v-dialog
-  box-shadow: none !important
 </style>
