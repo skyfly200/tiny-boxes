eb9107f90af329494d3cd45669358c2909f64764
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2020-12-21 21:47:16 -0700

    strip out unused stuff

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,18 @@
 <template lang="pug">
   v-app
-    Nav(v-if="this.$route.path !== '/'")
     v-content
       router-view
 </template>
 
 <script>
 import Vue from "vue";
-import Nav from "@/components/Nav.vue";
 
 export default Vue.extend({
   name: "App",
-  components: { Nav },
-  computed: {
-    page() {
-      const route = this.$route.name;
-      if (route === "Cell") return "Collection";
-      // show cell page as collection route
-      else return route;
-    }
-  }
 });
 </script>
 
 <style lang="sass">
-body
-  margin: 0
-  background-color: #121212
-  font-family: sans-serif
 .title
   font-family: "Pangolin"
   font-size: 2rem
