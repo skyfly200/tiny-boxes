587f3b4a16b7361c7853486bd792344f743a8a7a
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2020-12-28 20:47:42 -0700

    clip svg to viewbox

--- contracts/libraries/SVG.sol
+++ contracts/libraries/SVG.sol
@@ -85,7 +85,7 @@ library SVG {
     function _generateSVG(string memory body) internal pure returns (string memory) {
         string memory xmlVersion = '<?xml version="1.0" encoding="UTF-8"?>';
         string memory doctype = '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">';
-        string memory openingSVGTag = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="100%" height="100%" viewBox="0 0 2400 2400" style="stroke-width:0;background-color:#121212">';
+        string memory openingSVGTag = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="100%" height="100%" viewBox="0 0 2400 2400" preserveAspectRatio="xMidYMin slice" style="stroke-width:0;background-color:#121212">';
 
         return string(abi.encodePacked(xmlVersion, doctype, openingSVGTag, body, '</svg>'));
     }
