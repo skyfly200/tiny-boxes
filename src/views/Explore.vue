0d629b9a77da3e2ea95a92977e7716c7694600f8
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2021-01-06 12:27:53 -0700

    fix the explore page

--- src/views/Explore.vue
+++ src/views/Explore.vue
@@ -41,6 +41,22 @@ export default Vue.extend({
     t.loadFormDefaults();
   },
   methods: {
+    getSupply: function() {
+      return this.$store.state.contracts.tinyboxes.methods.totalSupply().call();
+    },
+    getPrice: function() {
+      return this.$store.state.contracts.tinyboxes.methods.currentPrice().call();
+    },
+    lookupLimit: async function() {
+      (this as any).limit = await this.$store.state.contracts.tinyboxes.methods.TOKEN_LIMIT().call();
+    },
+    loadStatus: async function() {
+      const t = this as any;
+      const idLookup = t.getSupply();
+      const priceLookup = t.getPrice();
+      t.id = await idLookup;
+      t.price = await priceLookup;
+    },
     randomize: function() {
       const t = this as any;
       const randomSettings: any = {};
@@ -98,6 +114,7 @@ export default Vue.extend({
     },
     loadTokens: async function() {
       this.loading = true;
+      await this.loadStatus()
       for (let t=0;t<this.count;t++) {
         this.randomize();
         this.$set(this.tokens, t, {
@@ -109,14 +126,11 @@ export default Vue.extend({
     },
     assemblePalette: function() {
       const v = (this as any).values;
-      v.lightness = v.lightness.sort((a: any,b: any) => a - b);
       return [
         v.hue,
         v.saturation,
-        v.lightness[0],
-        v.lightness[1],
-        v.scheme,
-        v.shades
+        v.lightness,
+        v.contrast <= v.lightness ? v.contrast : v.lightness
       ];
     },
     assembleDials: function() {
@@ -124,28 +138,17 @@ export default Vue.extend({
       const v = t.values;
       v.width = v.width.sort((a: any,b: any) => a - b);
       v.height = v.height.sort((a: any,b: any) => a - b);
-      return [
-        v.x,
-        v.y,
-        v.xSeg,
-        v.ySeg,
-        v.width[0],
-        v.width[1],
-        v.height[0],
-        v.height[1],
-        v.hatching,
-        v.mirrorAdv ? v.mirrorPos1 : (v.mirrorA ? t.defaults.mirrorPos1 : 0),
-        v.mirrorAdv ? v.mirrorPos2 : (v.mirrorB ? t.defaults.mirrorPos2 : 0),
-        v.mirrorAdv ? v.mirrorPos3 : (v.mirrorC ? t.defaults.mirrorPos3 : 0),
-        v.mirrorAdv ? v.scale : (!v.mirrorC ? (!v.mirrorB ? 400 : 200) : 100),
-      ];
+      return {
+        spacing: [ v.spread, (v.rows * 16) + v.cols ],
+        size: [ ...v.width, ...v.height ],
+      };
     },
     loadToken: function() {
       return new Promise((resolve, reject) => {
         const t = this as any;
-        const v = {...t.values, palette: t.assemblePalette(), dials: t.assembleDials()};
+        const v = {...t.values, ...t.assembleDials(), color: t.assemblePalette()};
         this.$store.state.contracts.tinyboxes.methods
-          .tokenTest(v.seed.toString(), v.shapes, v.palette, v.dials, v.animation, v.animate)
+          .tokenPreview(v.seed.toString(), v.shapes, v.hatching, v.color, v.size, v.spacing, v.traits, v.animate, t.id)
           .call()
           .then((result: any) => {
             t.data = result;
@@ -161,6 +164,7 @@ export default Vue.extend({
   },
   data: function() {
     return {
+      id: 0,
       loading: true,
       count: 60,
       tokens: [] as any,
@@ -176,21 +180,11 @@ export default Vue.extend({
         width: [200, 300],
         height: [200, 300],
         hatching: 0,
-        mirrorAdv: false,
-        mirrorA: true,
-        mirrorB: true,
-        mirrorC: true,
-        mirrorPos1: 600,
-        mirrorPos2: 1200,
-        mirrorPos3: 2400,
-        scale: 100,
         hue: Date.now() % 360,
         saturation: 80,
         lightness: [30,70],
-        shades: 3,
-        scheme: 0,
         animate: false,
-        animation: 9,
+        traits: [0,0,8,8,5],
       },
       sections: sections,
     };
