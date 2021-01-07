3592a5a1f91dd22f517ad6a1ac3814e2e4508594
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2021-01-06 21:46:49 -0700

    shrink ColorGrid squares

--- src/components/ColorsGrid.vue
+++ src/components/ColorsGrid.vue
@@ -1,8 +1,8 @@
 <template lang="pug">
     .colors-grid
-      svg(:width="(3*(parseInt(shades)+1)) + 'em'" :height="(3*(schemeH(scheme).length)) + 'em'")
+      svg(:width="(2.2*(parseInt(shades)+1)) + 'em'" :height="(2.2*(schemeH(scheme).length)) + 'em'")
         g(v-for="h,i of schemeH(scheme)")
-          rect(v-for="s in parseInt(shades)+1" :x="3*(s-1)+'em'" :y="3*i+'em'" width="3em" height="3em"
+          rect(v-for="s in parseInt(shades)+1" :x="2.2*(s-1)+'em'" :y="2.2*i+'em'" width="2.2em" height="2.2em"
             :style="'fill: hsl('+(parseInt(color[0])+h)+','+color[1]+'%,'+calcShade(s-1)+'%)'")
 </template>
 
