19c19a1d838a319ae60c091e9328c84230894ac9
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2021-01-09 03:04:38 -0700

    setup vue awesome countdown

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import vueAwesomeCountdown from "vue-awesome-countdown";
+
+Vue.use(vueAwesomeCountdown, 'vac') // Component name, `countdown` and `vac` by default
 
 Vue.config.productionTip = false;
 
