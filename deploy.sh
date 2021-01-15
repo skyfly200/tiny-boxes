0c25dbed12770cf0c2859fc8270cf999573a8cf5
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2021-01-15 13:44:45 -0700

    add a deploy bash scipt

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+trap "exit" INT TERM    # Convert INT and TERM to EXIT
+trap "kill 0" EXIT      # Kill all child processes if we receive EXIT
+
+RAND="0xa30E0997782fe8B4E888b22d711611fBCd0a388B"
+
+while getopts "d:i:c:a:s:" arg; do
+    case $arg in
+        c) ADDRESS=$OPTARG;;
+    esac
+done
+
+## if deploy or render
+## delete oz lock file ./.openzeppelin/.lock
+rm -f ./.openzeppelin/.lock
+
+ADDRESS=$(npx oz deploy -v --no-interactive -k regular -n rinkeby TinyBoxes "$RAND" | tail -n 1)
