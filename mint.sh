5b0f68575532c69966366c0ce7ec0f4d794bce22
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2021-01-19 12:19:29 -0700

    add to param to mint.sh

--- mint.sh
+++ mint.sh
@@ -3,10 +3,11 @@
 trap "exit" INT TERM    # Convert INT and TERM to EXIT
 trap "kill 0" EXIT      # Kill all child processes if we receive EXIT
 
-while getopts "c:f:" arg; do
+while getopts "c:f:t:" arg; do
     case $arg in
         c) ADDRESS=$OPTARG;;
         f) FROM=$OPTARG;;
+        t) TO=$OPTARG;;
     esac
 done
 
@@ -16,9 +17,15 @@ rm -f ./.openzeppelin/.lock
 
 npx oz send-tx -n rinkeby -v --method setPause --to $ADDRESS --args "false"
 
-if [ -z "$FROM" ]
+
+if [ -z "$TO" ]
     then
-        npx oz send-tx -n rinkeby -v --method createTo -f $FROM --to $ADDRESS --value 100000000000000000 --args "685525412542, 25, 5, [15,50,70], [100,100,100,100], [50,50], 63, 0x7A832c86002323a5de3a317b3281Eb88EC3b2C00, 0"
-    else
-        npx oz send-tx -n rinkeby -v --method createTo --to $ADDRESS --value 100000000000000000 --args "685525412542, 25, 5, [15,50,70], [100,100,100,100], [50,50], 63, 0x7A832c86002323a5de3a317b3281Eb88EC3b2C00, 0"
+        echo "must set to address with -t"
+    else        
+        if [ -z "$FROM" ]
+            then
+                npx oz send-tx -n rinkeby -v --method createTo --to $ADDRESS --value 100000000000000000 --args "685525412542, 25, 5, [15,50,70], [100,100,100,100], [50,50], 63, $TO, 0"
+            else
+                npx oz send-tx -n rinkeby -v --method createTo -f $FROM --to $ADDRESS --value 100000000000000000 --args "685525412542, 25, 5, [15,50,70], [100,100,100,100], [50,50], 63, $TO, 0" 
+        fi
 fi
