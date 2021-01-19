62ce8008abcdad2ac29e26fdecadc3bcfce10ed6
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2021-01-19 12:10:20 -0700

    add from option to the mint test script

--- mint.sh
+++ mint.sh
@@ -3,9 +3,10 @@
 trap "exit" INT TERM    # Convert INT and TERM to EXIT
 trap "kill 0" EXIT      # Kill all child processes if we receive EXIT
 
-while getopts "c:" arg; do
+while getopts "c:f:" arg; do
     case $arg in
         c) ADDRESS=$OPTARG;;
+        f) FROM=$OPTARG;;
     esac
 done
 
@@ -15,4 +16,9 @@ rm -f ./.openzeppelin/.lock
 
 npx oz send-tx -n rinkeby -v --method setPause --to $ADDRESS --args "false"
 
-npx oz send-tx -n rinkeby -v --method createTo --to $ADDRESS --value 100000000000000000 --args "685525412542, 25, 5, [15,50,70], [100,100,100,100], [50,50], 63, 0x7A832c86002323a5de3a317b3281Eb88EC3b2C00, 0"
+if [ -z "$FROM" ]
+    then
+        npx oz send-tx -n rinkeby -v --method createTo -f $FROM --to $ADDRESS --value 100000000000000000 --args "685525412542, 25, 5, [15,50,70], [100,100,100,100], [50,50], 63, 0x7A832c86002323a5de3a317b3281Eb88EC3b2C00, 0"
+    else
+        npx oz send-tx -n rinkeby -v --method createTo --to $ADDRESS --value 100000000000000000 --args "685525412542, 25, 5, [15,50,70], [100,100,100,100], [50,50], 63, 0x7A832c86002323a5de3a317b3281Eb88EC3b2C00, 0"
+fi
