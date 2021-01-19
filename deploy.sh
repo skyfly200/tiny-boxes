6f2f2cade85503ff330f02c8ab0ebed5e60862bd
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2021-01-18 23:45:43 -0700

    update deploy and mint scripts

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 trap "exit" INT TERM    # Convert INT and TERM to EXIT
 trap "kill 0" EXIT      # Kill all child processes if we receive EXIT
 
-RAND="0xa30E0997782fe8B4E888b22d711611fBCd0a388B"
+RAND="0x3c31a56DB3fc38e8960b77D5089f2e755DEEAFC3"
 
 ## if deploy or render
 ## delete oz lock file ./.openzeppelin/.lock
