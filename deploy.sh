5621044ca732a8c56c4e62e2f5feacbeb1c7cbe1
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2021-01-19 23:10:42 -0700

    update RAND in deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 trap "exit" INT TERM    # Convert INT and TERM to EXIT
 trap "kill 0" EXIT      # Kill all child processes if we receive EXIT
 
-RAND="0x6a05F48E86b1A77759DEeb4E69caf4ed6FcfBB56"
+RAND="0x02F597BFdB0291FE0789CA123D0dD9A2babfE845"
 
 ## if deploy or render
 ## delete oz lock file ./.openzeppelin/.lock
