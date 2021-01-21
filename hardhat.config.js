2eb8ce849b951833ac64908fe6497ca64a9fac30
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2021-01-21 11:33:59 -0700

    setup rinkeby network

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,7 @@
  * @type import('hardhat/config').HardhatUserConfig
  */
  require("@nomiclabs/hardhat-waffle");
+ const { projectId, mnemonic } = require('./secrets.json')
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -18,6 +19,13 @@ task("accounts", "Prints the list of accounts", async () => {
 
 
 module.exports = {
+  defaultNetwork: "rinkeby",
+  networks: {
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${projectId}`,
+      accounts: mnemonic
+    }
+  },
   solidity: {
     version: "0.6.8",
     settings: {
