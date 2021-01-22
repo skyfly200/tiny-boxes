7f4080db21dc6b15ef6b5d15e75183afc7ef8d05
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2021-01-22 02:15:16 -0700

    add web3 plugin and balance task

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,10 +3,19 @@
  */
 require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-web3");
+
 const { projectId, mnemonic } = require('./secrets.json')
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
+task("balance", "Prints an account's balance")
+  .addParam("account", "The account's address")
+  .setAction(async taskArgs => {
+    const account = web3.utils.toChecksumAddress(taskArgs.account);
+    const balance = await web3.eth.getBalance(account);
+
+    console.log(web3.utils.fromWei(balance, "ether"), "ETH");
+  });
+
 task("accounts", "Prints the list of accounts", async () => {
   const accounts = await ethers.getSigners();
 
