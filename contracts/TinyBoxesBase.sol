7e0441e7038f6ec0abd90e3714590fab732624d7
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2021-01-23 12:26:22 -0700

    remove comment

--- contracts/TinyBoxesBase.sol
+++ contracts/TinyBoxesBase.sol
@@ -2,9 +2,6 @@
 pragma solidity ^0.6.8;
 pragma experimental ABIEncoderV2;
 
-// needed for upgradability
-//import "@openzeppelin/upgrades/contracts/Initializable.sol";
-
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
