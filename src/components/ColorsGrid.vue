ea738bdd2da44cba0ab08f5a04294f98e1ee7cb9
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2021-01-30 12:05:05 -0700

    fix color and hue grids

--- src/components/ColorsGrid.vue
+++ src/components/ColorsGrid.vue
@@ -21,17 +21,18 @@ export default Vue.extend({
     },
     schemeH(i: number) {
       const schemes = [
-            [0, 120, 240], // triadic
-            [0, 180], // complimentary
-            [0, 60, 180, 240], // tetradic
-            [0, 30, 330], // analogous
-            [0, 30, 180, 330], // analogous and complimentary
-            [0, 150, 210], // split complimentary
-            [0, 150, 180, 210], // complimentary and analogous
-            [0, 30, 60, 90], // series
-            [0, 90, 180, 270], // square
-            [0], // mono
-        ];
+          [0, 30, 330], // analogous
+          [0, 120, 240], // triadic
+          [0, 180], // complimentary
+          [0, 60, 180, 240], // tetradic
+          [0, 30, 180, 330], // analogous and complimentary
+          [0, 150, 210], // split complimentary
+          [0, 150, 180, 210], // complimentary and analogous
+          [0, 30, 60, 90], // series
+          [0, 90, 180, 270], // square
+          [0], // mono
+          [0], // random
+      ];
       return schemes[i];
     },
   }
