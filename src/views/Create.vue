0890e2b7f00b6b778835190edf5b070058e68b2d
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2021-03-08 13:41:52 -0700

    preview with no contrast

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -486,7 +486,7 @@ export default Vue.extend({
       t.loading = true;
       await t.loadStatus()
       const v = {...t.values, ...t.assembleDials(), palette: t.assemblePalette(), settings: [0, 10, 0]};
-      const traits = [ 0, t.phase, 7, v.color.luminosity ];
+      const traits = [ 0, t.phase, 7, 0 ]; // v.color.luminosity
       this.$store.state.contracts.tinyboxes.methods
         .renderPreview(v.seed.toString(), v.palette, [v.shapes, v.hatching], v.size, v.spacing, v.mirroring, v.settings, traits, '')
         .call()
