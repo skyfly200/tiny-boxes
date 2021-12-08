3447c1f42d84102b44069b720c46cca9008f8142
G. Willson <greg@biomassiv.es>
2021-12-07 21:44:15 -0700

    Update tinyboxes_rarity_indexer.py

--- tinyboxes_rarity_indexer.py
+++ tinyboxes_rarity_indexer.py
@@ -2,65 +2,32 @@ import json
 import pymongo
 import numpy as np
 
-# dev notes   
-
-# shades is missing 6 and 7 data for some reason, reworking that
-# contrast is fixed. 
-
-# todo: 
-# rewrite clunky logic used for initial two traits- cols and rows.. :p
-# provide json file to be comsumed by tinyboxbot, format rarity report for each non- LE token 
-# -gw
-
-
+# dev notes
+#
+# shades is missing 6 and 7 data for some reason
+#
+ # -gw
 
 client = pymongo.MongoClient("localhost", 27017)
 mydb = client["tinybox_attributes"]
 myrec = mydb["minted_boxes"]
 
-
 with open('minted_box_rarity.json') as a:
     dict = json.load(a)
 
-colval1=0
-colval2=0
-colval3=0
-colval4=0
-colval5=0
-colval6=0
-colval7=0
-colval8=0
-colval9=0
-colval10=0
-colval11=0
-colval12=0
-colval13=0
-colval14=0
-colval15=0
-colval16=0
-rowval1=0
-rowval2=0
-rowval3=0
-rowval4=0
-rowval5=0
-rowval6=0
-rowval7=0
-rowval8=0
-rowval9=0
-rowval10=0
-rowval11=0
-rowval12=0
-rowval13=0
-rowval14=0
-rowval15=0
-rowval16=0
-
+rows = np.linspace(1,16,16)
+rows = rows.astype(int)
+rowscount = np.zeros(shape=16)
+rowscount = rowscount.astype(int)
+cols = np.linspace(1,16,16)
+cols = cols.astype(int)
+colscount = np.zeros(shape=16)
+colscount = colscount.astype(int)
 mirroring = np.array([(0,1,2),(0,1,3),(0,2,1),(0,2,3),(0,3,1),(0,3,2),(1,0,2),(1,0,3),(1,2,0),(1,2,3),(1,3,0),(1,3,2),(2,0,1),(2,0,3),(2,1,0),(2,1,3),(2,3,0),(2,3,1),(3,0,1),(3,0,2),(3,1,0),(3,1,2),(3,2,0),(3,2,1)])
-shapes = np.array([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30])
-
 mirroringcount = np.zeros(shape=24)
 mirroringcount = mirroringcount.astype(int)
-#shapes = np.zeros(shape=30)
+shapes = np.linspace(1,30,30)
+shapes = shapes.astype(int)
 shapescount = np.zeros(shape=30)
 shapescount = shapescount.astype(int)
 spread = np.linspace(0,100,101)
@@ -78,78 +45,14 @@ shadescount = np.zeros(shape=7)
 hatching = np.linspace(1,31,31)
 hatchingcount = np.zeros(shape=31)
 contrast = np.linspace(0,100,101)
-contrast=contrast.astype(int)
+contrast = contrast.astype(int)
 contrastcount = np.zeros(shape=101)
 contrastcount = contrastcount.astype(int)
 
-
 for j in range(1,1409):
-    colval = (dict[j]["trait_columns"])
-    if colval == 1:
-        colval1 = colval1 +1
-    if colval == 2:
-        colval2 = colval2 +1
-    if colval == 3:
-        colval3 = colval3 +1
-    if colval == 4:
-        colval4 = colval4 +1
-    if colval == 5:
-        colval5 = colval5 +1
-    if colval == 6:
-        colval6 = colval6 +1
-    if colval == 7:
-        colval7 = colval7 +1
-    if colval == 8:
-        colval8 = colval8 +1
-    if colval == 9:
-        colval9 = colval9 +1
-    if colval == 10:
-        colval10 = colval10 +1
-    if colval == 11:
-        colval11 = colval11 +1
-    if colval == 12:
-        colval12 = colval12 +1
-    if colval == 13:
-        colval13 = colval13 +1
-    if colval == 14:
-        colval14 = colval14 +1
-    if colval == 15:
-        colval15 = colval15 +1
-    if colval == 16:
-        colval16 = colval16 +1
-    rowval = (dict[j]["trait_rows"])
-    if rowval == 1:
-        rowval1 = rowval1 +1
-    if rowval == 2:
-        rowval2 = rowval2 +1
-    if rowval == 3:
-        rowval3 = rowval3 +1
-    if rowval == 4:
-        rowval4 = rowval4 +1
-    if rowval == 5:
-        rowval5 = rowval5 +1
-    if rowval == 6:
-        rowval6 = rowval6 +1
-    if rowval == 7:
-        rowval7 = rowval7 +1
-    if rowval == 8:
-        rowval8 = rowval8 +1
-    if rowval == 9:
-        rowval9 = rowval9 +1
-    if rowval == 10:
-        rowval10 = rowval10 +1
-    if rowval == 11:
-        rowval11 = rowval11 +1
-    if rowval == 12:
-        rowval12 = rowval12 +1
-    if rowval == 13:
-        rowval13 = rowval13 +1
-    if rowval == 14:
-        rowval14 = rowval14 +1
-    if rowval == 15:
-        rowval15 = rowval15 +1
-    if rowval == 16:
-        rowval16 = rowval16 +1
+
+    rowsval = (dict[j]["trait_rows"])
+    colsval = (dict[j]["trait_columns"])
     mirroringval = (dict[j]["trait_mirroring"])
     shapesval = (dict[j]["trait_shapes"])
     spreadval = (dict[j]["trait_spread"])
@@ -159,8 +62,18 @@ for j in range(1,1409):
     animationval = (dict[j]["trait_animation"])
     shadesval = (dict[j]["trait_shades"])
     hatchingval = (dict[j]["trait_hatching"])
-    contrastval = (dict[j]["trait_contrast"])
-#    contrastval = contrastval.rstrip(contrastval[-1])
+    contrastval = str((dict[j]["trait_contrast"]))
+    contrastval = contrastval.rstrip(contrastval[-1])
+
+    for k in range(0,len(rows)):
+        flag2 = rows[k]
+        if rowsval == flag2:
+            rowscount[k] = rowscount[k] +1
+
+    for k in range(0,len(cols)):
+        flag2 = cols[k]
+        if colsval == flag2:
+            colscount[k] = colscount[k] +1
 
     for k in range(0,len(mirroring)):   
         flag1 =  "[" + mirroringval.replace(","," ") + "]"
@@ -207,85 +120,19 @@ for j in range(1,1409):
             hatchingcount[k] = hatchingcount[k] +1
 
     for k in range(0,len(contrast)):
-        print ("contrast")
-        print (contrastval)
-        print ( contrast[k] )
-        flag2 = int(contrast[k])
+        flag2 = str(contrast[k])
         if contrastval == flag2:
             contrastcount[k] = contrastcount[k] +1
 
-
-
-
-
-
-
-print( "column:  1 count" )
-print( colval1 )        
-print( "column:  2 count" )
-print( colval2 )        
-print( "column:  3 count" )
-print( colval3 )
-print( "column:  4 count" )
-print( colval4 ) 
-print( "column:  5 count" )
-print( colval5 )
-print( "column:  6 count" )
-print( colval6 )
-print( "column:  7 count" )
-print( colval7 )                                       
-print( "column:  8 count" )
-print( colval8 )                                       
-print( "column:  9 count" )
-print( colval9 )                                       
-print( "column:  10 count" )
-print( colval10 )                                       
-print( "column:  11 count" )
-print( colval11 )                                       
-print( "column:  12 count" )
-print( colval12 )                                       
-print( "column:  13 count" )
-print( colval13 )                                       
-print( "column:  14 count" )
-print( colval14 )                                       
-print( "column:  15 count" )
-print( colval15 )                                       
-print( "column:  16 count" )
-print( colval16 )                                       
-print( "row:  1 count" )
-print( rowval1 )        
-print( "row:  2 count" )
-print( rowval2 )        
-print( "row:  3 count" )
-print( rowval3 )        
-print( "row:  4 count" )
-print( rowval4 )        
-print( "row:  5 count" )
-print( rowval5 )        
-print( "row:  6 count" )
-print( rowval6 )        
-print( "row:  7 count" )
-print( rowval7 )        
-print( "row:  8 count" )
-print( rowval8 )        
-print( "row:  9 count" )
-print( rowval9 )        
-print( "row:  10 count" )
-print( rowval10 )        
-print( "row:  11 count" )
-print( rowval11 )        
-print( "row:  12 count" )
-print( rowval12 )        
-print( "row:  13 count" )
-print( rowval13 )        
-print( "row:  14 count" )
-print( rowval14 )        
-print( "row:  15 count" )
-print( rowval15 )        
-print( "row:  16 count" )
-print( rowval16 )        
-
-       
+for k in range(0,len(cols)):
+    print ( "cols: " )
+    print ( cols[k] )
+    print ( colscount[k] )
+  
+for k in range(0,len(rows)):
+    print ( "rows: " )
+    print ( rows[k] )
+    print ( rowscount[k] )
 
 for k in range(0,len(mirroring)):
     print ( "mirroring: " )
