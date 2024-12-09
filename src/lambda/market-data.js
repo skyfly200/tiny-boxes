573c2da79449e8ab39038f3731978bf7854b59e4
Skyler Fly-Wilson <magic@skylerfly.com>
2024-12-09 14:50:53 -0700

    add market-data endpoint

---
+++ src/lambda/market-data.js
@@ -0,0 +1,26 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+const {
+    ALCHEMY_API_KEY,
+    CONTRACT_ADDRESS,
+} = process.env
+
+exports.handler = async (event, context) => {
+    // wrap things with error catching
+    try {
+        const options = {method: 'GET', headers: {accept: 'application/json'}};
+
+        const urlFloor = 'https://eth-mainnet.g.alchemy.com/nft/v3/' + 'docs-demo' + '/getFloorPrice?contractAddress=' + CONTRACT_ADDRESS
+        resFloor = await fetch(urlFloor, options)
+
+        const urlSales = 'https://eth-mainnet.g.alchemy.com/nft/v3/' + 'docs-demo' + '/getNFTSales?contractAddress=' + CONTRACT_ADDRESS
+        resSales = await fetch(urlSales, options)
+
+        return generateResponse({floor: resFloor.json(), sales: resSales.json()}, 200)
+        
+    } catch (err) {
+      console.log(err)
+      return generateResponse('Server Error', 500)
+    }
+}
